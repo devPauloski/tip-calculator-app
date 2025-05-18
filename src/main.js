f8730bcd3bbbb496085544034482f318e2eb2b8a
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-05-18 15:08:42 +0100

    Made changes

--- src/main.js
+++ src/main.js
@@ -5,12 +5,18 @@ const tipOptions = document.querySelectorAll(".tip-option");
 const outputTipValue = document.querySelector(".output-tip-value");
 const outputTotalValue = document.querySelector(".output-total-value");
 
-peopleInput.addEventListener("input", () => {
-  peopleInput.value = peopleInput.value.trim();
-});
+billInput.addEventListener("input", () => {
+  let billValue = billInput.value.replace(/[^0-9.]/g, "");
+  let billValueSplit = billValue.split(".");
 
-billInput.addEventListener("focusout", () => {
-  billInput.value = new Intl.NumberFormat().format(billInput.value.trim());
+  if (billValueSplit.length >= 2) {
+    billValue = billValueSplit[0] + "." + billValueSplit[1].slice(0, 2);
+  }
+
+  billInput.value = new Intl.NumberFormat("en-US", {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2
+  }).format(billValue);
 });
 
 form.addEventListener("submit", (event) => {
