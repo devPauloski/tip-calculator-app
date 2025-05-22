aa5ec315420e4e69bdf76f5ceefc16f175d465de
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-05-22 12:38:10 +0100

    Made changes

--- src/main.js
+++ src/main.js
@@ -10,12 +10,12 @@ let selectedTipValue = "";
 
 billInput.addEventListener("input", (event) => {
   const input = event.target;
-  let billValue = input.value.replace(/[^0-9.]/g, '');
+  let billValue = input.value.replace(/[^0-9.]/g, "");
 
   const splitValueByDot = billValue.split(".");
   // keep only one dot
   if (splitValueByDot.length > 2) {
-    billValue = splitValueByDot[0] + "." + splitValueByDot[1]; 
+    billValue = splitValueByDot[0] + "." + splitValueByDot[1];
   }
 
   // Limit to 2 decimal places
@@ -27,68 +27,76 @@ billInput.addEventListener("input", (event) => {
   const number = parseFloat(billValue);
 
   if (!isNaN(number)) {
-    const [integerPart, decimalPart] = billValue.split('.');
-    input.value = new Intl.NumberFormat().format(parseInt(integerPart)) + (decimalPart !== undefined ? '.' + decimalPart : '');
+    const [integerPart, decimalPart] = billValue.split(".");
+    input.value =
+      new Intl.NumberFormat().format(parseInt(integerPart)) +
+      (decimalPart !== undefined ? "." + decimalPart : "");
   } else {
     input.value = "";
   }
+
+  computeTipResult();
 });
 
 peopleInput.addEventListener("input", (event) => {
   const input = event.target;
-  let peopleValue = input.value.replace(/[^0-9]/g, '');
+  let peopleValue = input.value.replace(/[^0-9]/g, "");
   input.value = peopleValue;
+  computeTipResult();
 });
 
 tipButtons.forEach((tipButton) => {
   tipButton.addEventListener("click", (event) => {
     let selectedTipButton = event.target;
     selectedTipValue = parseFloat(selectedTipButton.dataset.value);
-    
-    tipButtons.forEach(tipButton => tipButton.classList.remove("selected"));
+
+    tipButtons.forEach((tipButton) => tipButton.classList.remove("selected"));
     selectedTipButton.classList.add("selected");
 
     customTipInput.value = "";
-  })
+
+    computeTipResult();
+  });
 });
 
 customTipInput.addEventListener("input", (event) => {
   const input = event.target;
-  let customTipValue = input.value.replace(/[^0-9]/g, '');
+  let customTipValue = input.value.replace(/[^0-9]/g, "");
   input.value = customTipValue;
 
   if (input.value) {
     selectedTipValue = parsefloat(input.value) / 100;
-    tipButtons.forEach(tipButton => tipButton.classList.remove("selected"));
-  } 
+    tipButtons.forEach((tipButton) => tipButton.classList.remove("selected"));
+  } else {
+    selectedTipValue = "";
+  }
+
+  computeTipResult();
 });
 
-form.addEventListener("input", (event) => {
-  event.preventDefault();
+const computeTipResult = () => {
   let validInput = true;
 
-  if (billInput.value === "") {
-    validInput = false;
-  }
-
-  if (selectedTipValue === "") {
-    validInput = false;
-  }
-
-  if (peopleInput.value === "" || peopleInput.value === "0") {
-    validInput = false;
-  }
+  if (billInput.value === "") validInput = false;
+  if (selectedTipValue === "") validInput = false;
+  if (peopleInput.value === "" || peopleInput.value === "0") validInput = false;
 
   if (validInput) {
     const billAmount = parseFloat(billInput.value.replaceAll(",", ""));
     const tipPercent = selectedTipValue;
     const peopleCount = parseFloat(peopleInput.value);
 
-    const tipAmountPerPerson = ((tipPercent * billAmount) / peopleCount).toFixed(2);
-    const totalAmountPerPerson = (((1 + tipPercent) * billAmount) / peopleCount).toFixed(2);
+    const tipAmountPerPerson = new Intl.NumberFormat().format(
+      ((tipPercent * billAmount) / peopleCount).toFixed(2)
+    );
+    const totalAmountPerPerson = new Intl.NumberFormat().format(
+      (((1 + tipPercent) * billAmount) / peopleCount).toFixed(2)
+    );
 
     outputTipValue.textContent = `$${tipAmountPerPerson}`;
     outputTotalValue.textContent = `$${totalAmountPerPerson}`;
-    console.log(tipPercent);
-  }  
-});
+  } else {
+    outputTipValue.textContent = "$0.00";
+    outputTotalValue.textContent = "$0.00";
+  }
+};
